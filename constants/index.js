a07d9e4132d76bca8f8a51dbb8b51c8392edf023
unknowncryptoguy <unknowncrytoguy@gmail.com>
2023-11-17 02:26:45 +0800

    change url

--- constants/index.js
+++ constants/index.js
@@ -4,4 +4,4 @@ export const AUTH_TOKEN = "auth_token";
 
 export const BASE_URL = isDev
   ? "http://localhost:8080"
-  : "https://creso-b02eab9f8c40.herokuapp.com/";
+  : "https://creso-b02eab9f8c40.herokuapp.com";

2b7cdefea1e603055c91c1f0acbc55ca3a58f5f9
codebyMoh <codebymohit@gmail.com>
2023-12-26 18:58:19 +0530

    env change

--- constants/index.js
+++ constants/index.js
@@ -2,11 +2,11 @@ const isDev = process.env.NODE_ENV === "development";
 
 export const AUTH_TOKEN = "auth_token";
 
-export const BASE_URL = isDev
-  ? "https://creso-b02eab9f8c40.herokuapp.com"
-  : "http://localhost:8080"
+// export const BASE_URL = isDev
+//   ? "https://creso-b02eab9f8c40.herokuapp.com"
+//   : "http://localhost:8080"
 
-// export const BASE_URL = "https://creso-b02eab9f8c40.herokuapp.com";
+export const BASE_URL = "https://creso-b02eab9f8c40.herokuapp.com";
 
 export const RPC_URL_GOERLI =
   "https://goerli.infura.io/v3/a0b74d65173042fabe9639289bd336b5";

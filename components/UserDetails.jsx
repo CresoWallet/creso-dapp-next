b98ccb655915ff964ebf6caf3fcbc5880692c1cd
rudreshTanthetaa <rudreshr@tanthetaa.co.in>
2023-12-26 17:17:16 +0530

    Update

--- components/UserDetails.jsx
+++ components/UserDetails.jsx
@@ -5,14 +5,23 @@ import { useUser } from "@/providers/UserProvider";
 
 const UserDetails = () => {
   const { user, isAuthenticated, status } = useUser();
+
+  function getInitials(username) {
+    return username.substring(0, 2).toUpperCase();
+  }
   return (
     <div className="flex flex-row gap-2 items-center">
-      <Image alt="" src={User} />
+      <div className=" top-1 right-1 bg-red-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
+        <p className=" font-semibold text-xs">
+          {user ? getInitials(user.username) : ""}
+        </p>
+      </div>
+      {/* <Image alt="" src={User} /> */}
       <div className="flex flex-col space-y-1">
         <p className="font-bold text-xl">{user?.username}</p>
         <p className="text-sm text-[#A09FAA]">{user?.email}</p>
         <p className="text-xs text-[#A09FAA] mr-xside">
-          Last Backup :  <span className="text-sm text-black">28 OCT 2023</span>{" "}
+          Last Backup : <span className="text-sm text-black">28 OCT 2023 </span>{" "}
         </p>
       </div>
     </div>

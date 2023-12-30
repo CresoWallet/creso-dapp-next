01277a8ccca81f31ec4d6ea794439933b0e0aa6e
codebyMoh <codebymohit@gmail.com>
2023-12-30 11:45:56 +0530

    creso logo chnage

--- components/navbar/SideNav.jsx
+++ components/navbar/SideNav.jsx
@@ -19,8 +19,8 @@ import Telegram from "../../assets/Dashboard/telegram.png";
 import Etherscan from "../../assets/Dashboard/etherscan.png";
 import Discord from "../../assets/Dashboard/github.png";
 import Github from "../../assets/Dashboard/discord.png";
-import cresow from "../../assets/Dashboard/creso logo white.svg"
-import cresob from "../../assets/Dashboard/creso logo black.svg"
+import cresow from "../../assets/Dashboard/creso_logo_white.svg";
+import cresob from "../../assets/Dashboard/creso logo black.svg";
 
 const SideNav = () => {
   const pathName = usePathname();
@@ -34,9 +34,9 @@ const SideNav = () => {
   return (
     <div className="bg-[#2100EC] rounded-3xl flex flex-col pt-12 min-h-screen lg:h-full items-center lg:items-start  ">
       <div className="flex flex-row items-center gap-1 px-8 relative Mainlogo text-white">
-        <Image alt="creso-logo" src={cresow} className=" " />
-        
-{/* 
+        <Image alt="creso-logo" src={cresow} className="h-10 my-8 mb-14" />
+
+        {/* 
         <p className=" text-4xl hidden lg:block items-center">creso</p>
         <p className="text-sm mt-4 text-[#D0F500]">Beta</p>
 

846d663140dfd96c521bf01eaffd6cc52e9169d7
codebyMoh <codebymohit@gmail.com>
2024-01-01 16:59:03 +0530

    Scroll bar set and side bar make perfect from ankita

--- components/Mainnet.jsx
+++ components/Mainnet.jsx
@@ -243,11 +243,10 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
         <div className="flex xl:flex-row flex-col items-center xl:gap-4 md:gap-4 gap-2">
           {/* Keyless Secure Wallet */}
           <div
-            className={`${
-              activeButton === "AA"
-                ? "bg-black"
-                : "bg-white hover:bg-gray-200 duration-500 "
-            } rounded-full px-4 py-4 w-full border-2 border-black cursor-pointer group relative`}
+            className={`${activeButton === "AA"
+              ? "bg-black"
+              : "bg-white hover:bg-gray-200 duration-500 "
+              } rounded-full px-4 py-4 w-full border-2 border-black cursor-pointer group relative`}
           >
             <div className="flex flex-row justify-between items-center gap-3 group">
               <Image
@@ -257,9 +256,8 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
               />
               <div className="flex flex-col space-y-1">
                 <p
-                  className={`${
-                    activeButton === "AA" ? "text-white" : "text-black"
-                  }  font-semibold text-sm md:text-lg xl:text-sm`}
+                  className={`${activeButton === "AA" ? "text-white" : "text-black"
+                    }  font-semibold text-sm md:text-lg xl:text-sm`}
                 >
                   Keyless Secure Wallet
                 </p>
@@ -269,9 +267,8 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
                   </p>
                   <Image
                     src={activeButton === "AA" ? Copy : Copy2}
-                    className={`${
-                      activeButton === "AA" ? "text-white" : "text-black"
-                    }`}
+                    className={`${activeButton === "AA" ? "text-white" : "text-black"
+                      }`}
                     alt="copy"
                     onClick={() => {
                       copyToClipBoard(secureWalletAddress);
@@ -301,11 +298,10 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
 
           {/* EOA Wallet */}
           <div
-            className={`${
-              activeButton === "EOA"
-                ? "bg-black"
-                : "bg-white  hover:bg-gray-200 duration-500 "
-            } rounded-full px-4 py-4 w-full border-2 border-black cursor-pointer group relative`}
+            className={`${activeButton === "EOA"
+              ? "bg-black"
+              : "bg-white  hover:bg-gray-200 duration-500 "
+              } rounded-full px-4 py-4 w-full border-2 border-black cursor-pointer group relative`}
           >
             <div className="flex flex-row justify-between items-center gap-3">
               <Image
@@ -315,9 +311,8 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
               />
               <div className="flex flex-col space -y-1">
                 <p
-                  className={`${
-                    activeButton === "EOA" ? "text-white" : "text-black"
-                  }  font-semibold text-sm md:text-lg xl:text-sm`}
+                  className={`${activeButton === "EOA" ? "text-white" : "text-black"
+                    }  font-semibold text-sm md:text-lg xl:text-sm`}
                 >
                   EOA Wallet
                 </p>
@@ -355,7 +350,7 @@ const Mainnet = ({ handleOpenWallet, handleCreateWallet, showWallet }) => {
           </div>
         </div>
       </div>
-      <div className="overflow-y-auto custom-scrollbar h-64">
+      <div className="overflow-auto custom-scrollbar h-64">
         {isLoading ? (
           <>
             {arr.map((e, index) => {

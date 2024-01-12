2fbbb823bc71b3231d870d4196131fa24c890adb
codebyMoh <codebymohit@gmail.com>
2024-01-12 17:11:32 +0530

    Update

--- components/cards/Coin.jsx
+++ components/cards/Coin.jsx
@@ -1,81 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import Image from "next/image";
-// import Account from "../../assets/Dashboard/account.png";
-// import Add from "../../assets/Dashboard/add.png";
-// import ETH from "../../assets/Dashboard/ethSelect.png";
-// import USDT from "../../assets/Dashboard/usdt.png";
-// import DAI from "../../assets/Dashboard/dai.png";
-// import BnB from "../../assets/Dashboard/bnb.png";
-// import WETH from "../../assets/Dashboard/weth.png";
-// import { coinList } from "@/utils/data/coinlist";
 
-// const CoinCard = ({ handleCoinWallet }) => {
-//   const [usdRate, setUsdRate] = useState();
-
-//   useEffect(() => {
-//     const fetchUsdValue = async () => {
-//       try {
-//         const res = await getUSDValue();
-//         if (res) {
-//           setUsdRate({
-//             USDT: res?.data?.tether.usd,
-//             USDC: res?.data["usd-coin"].usd,
-//             WLD: res?.data?.worldcoin.usd,
-//             OKB: res?.data?.okb.usd,
-//             BNB: res?.data?.binancecoin.usd,
-//           });
-//         }
-//       } catch (error) {
-//         console.log("error : ", error);
-//       }
-//     };
-
-//     fetchUsdValue();
-//   }, []);
-//   return (
-//     <div className="flex gap-2 xl:gap-4 justify-between items-center overflow-x-auto ">
-//       {coinList.map((item, index) => {
-//         return (
-//           <div
-//             className="md:flex flex-col space-y-1 items-center cursor-pointer hover:-translate-y-1 duration-500 !overflow-hidden "
-//             onClick={(e) => {
-//               handleCoinWallet({ item: item });
-//             }}
-//             key={index}
-//           >
-//             <Image
-//               alt=""
-//               src={
-//                 item.coinName === "ETH"
-//                   ? ETH
-//                   : item.coinName === "BNB"
-//                     ? BnB
-//                     : item.coinName === "USDT"
-//                       ? USDT
-//                       : item.coinName === "DAI"
-//                         ? DAI
-//                         : item.coinName === "WETH"
-//                           ? WETH
-//                           : ETH
-//               }
-//               className="xl:h-14 xl:w-14 md:h-12 md:w-12 w-12 h-12"
-//             />
-//             <div className="flex flex-col">
-//               <p className="text-center xl:text-sm text-xs md:text-xs">
-//                 {item.coinName}
-//               </p>
-//               <p className="text-[#A09FAA] xl:text-sm text-xs md:text-xs">
-//                 {item.value}
-//               </p>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default CoinCard;
 
 /* eslint-disable @next/next/no-img-element */
 import React, { useContext, useEffect, useRef, useState } from "react";
@@ -91,30 +14,6 @@ const CoinCard = ({ handleCoinWallet }) => {
   const isLaptop = useMediaQuery({ query: `(max-width: 1024px)` });
   console.log("🚀 ~ CoinCard ~ originalData:", originalData)
 
-  // console.log("🚀 ~ getTokensPrice:", getTokensPrice)
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const tokensPerPage = 10;
-  // const startIndex = (currentPage - 1) * tokensPerPage;
-  // const endIndex = startIndex + tokensPerPage;
-  // const currentData = originalData.slice(startIndex, endIndex);
-  // console.log("🚀 ~ currentData:", currentData)
-
-  // const pricedata = currentData.map((e) => e.address);
-  // const top10Data = originalData.slice(0, 10);
-
-  // const pricedata = top10Data.map(e => e.address)
-
-  // const gettokenprice = async () => {
-  //   const Tokenprice = await axios.get(
-  //     `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${pricedata}&vs_currencies=usd`
-  //   );
-  //   // console.log("🚀 ~ gettokenprice ~ Tokenprice:", Tokenprice?.data)
-  //   setGetTokensPrice(Tokenprice?.data)
-  // };
-
-  // useEffect(() => {
-  //   gettokenprice()
-  // }, [currentPage])
 
 
   const top10Token = originalData.slice(0, 7);
@@ -128,9 +27,10 @@ const CoinCard = ({ handleCoinWallet }) => {
   // const endIndex = showAllTokens ? originalData.length : startIndex + tokensPerPage;
   // const currentData = originalData.slice(startIndex, endIndex);
   const handleSeeMore = () => {
-    setShowAllTokens(true);
+    // setShowAllTokens(true);
     setOpenPopup(true);
   };
+
   // Object.entries(getTokensPrice).map((e) => {
   //   console.log("🚀 ~ e 😂😂😁😁😂😁:", e)
   //   return e
@@ -187,7 +87,7 @@ const CoinCard = ({ handleCoinWallet }) => {
           className="text-[#FF4085] cursor-pointer hover:font-semibold"
           onClick={handleSeeMore}
         >
-          See More
+          See More 
         </div> : <div
           className="text-[#FF4085] font-semibold w-full text-center"
         >

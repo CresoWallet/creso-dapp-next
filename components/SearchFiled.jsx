699b838a64cb6814e078c0a4d0741414764e6592
codebyMoh <codebymohit@gmail.com>
2024-01-17 11:23:20 +0530

    Task 3 and Task 5

--- components/SearchFiled.jsx
+++ components/SearchFiled.jsx
@@ -31,15 +31,16 @@ const SearchField = ({ onSearch }) => {
   }, []);
 
   return (
-    <div className="flex items-center relative">
+    <div className="flex items-center relative h-12 ">
       {isSearchOpen ? (
         <div className="relative" ref={inputRef}>
           <input
             type="text"
             placeholder="Search..."
-            className="h-8 md:h-10 w-30 px-4 rounded-3xl border cursor-pointer hover:border-black ml-2 text-sm md:text-base"
+            className="h-10 md:h-10 w-full md:w-30 px-4 md:px-6 rounded-3xl border cursor-pointer hover:border-black ml-2 text-sm md:text-base"
             onChange={handleInputChange}
           />
+
           <CiSearch
             className="absolute right-3 top-2 text-gray-500 cursor-pointer hover:font-bold"
             onClick={handleSearchIconClick}
